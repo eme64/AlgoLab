b1aff5b2894b2f2e5d89dda1070a4d57657704d5
Emanuel Peter <emanuel.peter@hotmail.com>
2019-10-21 09:57:00 +0200

    w05/boats wip

--- w05/boats/code.cpp
+++ w05/boats/code.cpp
@@ -35,28 +35,96 @@ bool operator < (const Event& e1, const Event& e2) {
    return (e1.pos < e2.pos) or (e1.pos == e2.pos and e1.isBegin==false and e2.isBegin==true);
 };
 
+struct Element {
+   int pos;
+   int index;
+   Element(int pos,int index) : pos(pos),index(index) {};
+};
+bool operator < (const Element& e1, const Element& e2) {
+   return e1.pos < e2.pos;
+};
 struct Test {
    int n;
    std::vector<int> p;
    std::vector<int> l;
+   std::vector<bool> marked;
    std::vector<Event> events;
    int boundary;
+   std::priority_queue<Element> active1; // contains boat for p if boundary <= p-l
+   std::priority_queue<Element> contactList; // contains boat for p-l if boundary <= p-l
+   std::priority_queue<Element> active2; // contains boat for l if boundary > p-l
    
+   int count;
+
+   const Element nullEl(INT_MAX,-1);
+
    void read() {
       std::cin >> n;
       events.reserve(2*n);
       l.resize(n);
       p.resize(n);
+      marked.resize(n);
       for(int i=0;i<n;i++) {
 	 std::cin >> l[i] >> p[i];
          events.push_back(Event(i,true,p[i]-l[i]));
          events.push_back(Event(i,false,p[i]+l[i]));
+	 marked[i] = false;
       }
       std::sort(events.begin(),events.end());
       boundary = events[0].pos -1;
+      count = 0;
    };
    
    void insert(int i) {
+      // decide weather in active 1 or 2:
+      if(boundary <= p[i]-l[i]) {
+         active1.push(Element(p[i],i));
+         contactList.push(Element(p[i]-l[i],i));
+      } else {
+         active2.push(Element(l[i],i));
+      }
+   };
+   
+   Element& peekMin(std::priority_queue<Element> &q) {
+      // peekMin, but remove marked Elements
+      while(q.top()!=q.end()) {
+         Element &e = q.top();
+	 if(marked[e.index]) {
+	    q.pop();
+	 } else {
+	    return e;
+	 }
+      }
+      return nullEl;
+   }
+
+   int peekMin() {
+      // see what minimum end value is
+      int minEnd(INT_MAX);
+      Element &e1 = peekMin(active1);
+      if(e1!=nullEl) {
+         minEnd = e1.pos;
+      }
+      Element &e2 = peekMin(active2);
+      
+   } 
+
+   void remove(int i) {
+      // check if i could still work:
+      if(boundary <= p[i]) {
+         // could take i, or something better
+         // take as much up to p+l
+	 int top = p[i]+l[i];
+
+	 while() {}
+	 //std::cout << "take " << i << std::endl;
+	 //int boundaryI = std::max(boundary + l[i], p[i]);
+	 //count++;
+      } else {
+         // cannot take i
+	 std::cout << "reject " << i << std::endl;
+      }
+      marked[i] = true;
    };
 
    void run() {
@@ -69,6 +137,7 @@ struct Test {
 	 } else {
 	    // handle end
             std::cout << "end " << ev.index << std::endl;
+	    remove(i);
 	 }
       }
    };
@@ -85,5 +154,6 @@ int main () {
    for(int tt=0; tt<t; tt++) {
       test.read();
       test.run();
+      std::cout << test.count << std::endl;
    }
 }
