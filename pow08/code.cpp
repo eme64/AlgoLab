b77863ca3914ffe5d93efb33573e688e20e118f2
peterem <emanuel.peter@hotmail.com>
2020-01-23 12:30:57 +0100

    pow08 / correct

--- pow08/code.cpp
+++ pow08/code.cpp
@@ -44,32 +44,26 @@ struct Canton {
    std::vector<int> p;
    int z,j;
    std::vector<bool> M;
-
-   std::vector<edge_desc> zoneEdge;
+   
+   std::vector<bool> activeJ;
+   std::vector<bool> activeZ;
    
    int ret;
 
    Canton() {
       std::cin >> z >> j;
 
-      graph G(z+j+2);
-      edge_adder adder(G);
-      int src = z+j;
-      int dst = z+j+1;
-      
+      activeJ.resize(j,true);
+      activeZ.resize(z,true);
+
       c.resize(z);
-      zoneEdge.reserve(z);
-      edge_desc e;
       for(int i=0; i<z; i++) {
 	 std::cin >> c[i];
-	 e = adder.add_edge(src,i, c[i]);
-	 zoneEdge.push_back(e);
       }
 
       p.resize(j);
       for(int i=0; i<j; i++) {
 	 std::cin >> p[i];
-	 adder.add_edge(i+z,dst, p[i]);
       }
       
       M.resize(z*j,false);
@@ -79,8 +73,57 @@ struct Canton {
 	 for(int n=0; n<ni; n++) {
 	    int zz;
 	    std::cin >> zz;
-	    adder.add_edge(zz, i+z, (1<<30));
 	    M[i*j + zz] = true;
+	 }
+      }
+
+      while(not filter()) {}
+
+      long cost = 0;
+      for(int i=0; i<z; i++) {
+         if(activeZ[i]) {cost+=c[i];}
+      }
+      long profit = 0;
+      for(int i=0; i<j; i++) {
+         if(activeJ[i]) {profit+=p[i];}
+      }
+
+      ret = profit - cost;
+      assert(ret>=0);
+   }
+
+   bool filter() {
+      // returns true as soon as all active zones saturated
+      bool success = true;
+
+      graph G(z+j+2);
+      edge_adder adder(G);
+      int src = z+j;
+      int dst = z+j+1;
+
+      std::vector<edge_desc> zoneEdge;
+      zoneEdge.resize(z);
+      edge_desc e;
+      for(int i=0; i<z; i++) {
+	 if(activeZ[i]) {
+	    e = adder.add_edge(src,i, c[i]);
+	    zoneEdge[i] = e;
+	 }
+      }
+
+      for(int i=0; i<j; i++) {
+	 if(activeJ[i]) {
+	    adder.add_edge(i+z,dst, p[i]);
+         }
+      }
+      
+      for(int i=0; i<j; i++) {
+	 if(activeJ[i]) {
+            for(int zz=0; zz<z; zz++) {
+	       if(activeZ[zz] and M[i*j + zz]) {
+	          adder.add_edge(zz, i+z, (1<<30));
+	       }
+	    }
 	 }
       }
 
@@ -88,42 +131,31 @@ struct Canton {
       const auto c_map = boost::get(boost::edge_capacity, G);
       const auto rc_map = boost::get(boost::edge_residual_capacity, G);
 
-      std::vector<bool> takeZone(z,false);
       for(int i=0; i<z; i++) {
-         e = zoneEdge[i];
-	 long ff = c_map[e] - rc_map[e];
-	 if(ff == c[i]) {takeZone[i] = true;}
+         if(activeZ[i]) {
+	    e = zoneEdge[i];
+	    long ff = c_map[e] - rc_map[e];
+	    if(ff < c[i]) {activeZ[i] = false; success = false;}
+	 }
       }
 
       long profit = 0;
       long cost = 0;
-      std::vector<bool> reallyTakeZone(z,false);
       for(int i=0; i<j; i++) {
-         bool take = true;
-	 for(int zz=0; zz<z; zz++) {
-	    if(M[i*j + zz] and not takeZone[zz]) {take = false;}
-	 }
-
-	 if(take) {
-            profit+=p[i];
-	    std::cout << "take j " << i << std::endl;
+         if(activeJ[i]) {
+	    bool take = true;
 	    for(int zz=0; zz<z; zz++) {
-	       if(M[i*j + zz]) {
-	          if(not reallyTakeZone[zz]) {
-		     reallyTakeZone[zz] = true;
-		     cost += c[zz];
-	             std::cout << "take z " << zz << std::endl;
-		  }
-	       }
+	       if(M[i*j + zz] and not activeZ[zz]) {take = false;}
+	    }
+            
+	    if(not take) {
+	       activeJ[i] = false;
+	       success = false;
 	    }
 	 }
       }
-     
-      std::cout << "f: " << flow << std::endl;
-      std::cout << "p: " << profit << " c: " << cost << std::endl; 
-
-      ret = profit-cost;
-      assert(ret>=0);
+      //std::cout << "f: " << flow << " s: " << success << std::endl;
+      return success;
    }
 
    int run() {
